8db7eb254c79e254b5e5db60b05a5efaff229ed1
venomnotdead <vaidikchauhan333@gmail.com>
2024-09-13 19:35:55 +0530

    updated styling for product card

--- src/components/rendered/product/productCard.tsx
+++ src/components/rendered/product/productCard.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import Image from 'next/image';
 import { Product } from './productList';
+import {
+    ToggleGroup,
+    ToggleGroupItem,
+} from "@/components/ui/toggle-group"
+import { Icon } from "@iconify/react"
 
-const ProductCard = ({ product }: Product) => {
+const ProductCard = ({ product }: { product: Product }) => {
     return (
-        <div key={product.id} className="border rounded-lg shadow-lg overflow-hidden">
-            <Image src={product.thumbnail} alt={product.title} width={200} height={200} className="w-full h-40 object-cover" />
-            <div className="p-4">
-                <h2 className="text-xl font-semibold mb-2">{product.title}</h2>
-                <p className="text-gray-600">${product.price.toFixed(2)}</p>
+        <div key={product.id} className="relative border border-gray-200 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+
+            <Image
+                src={product.thumbnail}
+                alt={product.title}
+                width={400}
+                height={400}
+                className="w-full h-40 object-cover"
+            />
+            <div className='flex flex-col'>
+                <div className="p-4 bg-white">
+                    <h2 className="text-lg font-semibold mb-2 text-gray-800">{product.title}</h2>
+                    <p className="text-primary text-xl font-semibold">${product.price.toFixed(2)}</p>
+                </div>
+                <div>
+                    <ToggleGroup type="multiple">
+                        <ToggleGroupItem value="bold" aria-label="Toggle favorite" className="hover:bg-gray-200 transition">
+                            <Icon icon="uit:favorite" width="24" height="24" className="text-red-500" />
+                        </ToggleGroupItem>
+                        <ToggleGroupItem value="italic" aria-label="Toggle cart" className="hover:bg-gray-200 transition">
+                            <Icon icon="ion:cart" width="24" height="24" className="text-blue-500" />
+                        </ToggleGroupItem>
+                    </ToggleGroup>
+                </div>
             </div>
         </div>
     )
