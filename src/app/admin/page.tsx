6aa7b0fb9792fb9fa48be5a46dc0c3ac634e170d
venomnotdead <vaidikchauhan333@gmail.com>
2024-09-14 23:00:56 +0530

    updated admin ui and fix login issue

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,6 +4,12 @@ import { Input } from '@/components/ui/input'
 import React, { useEffect, useState } from 'react'
 import { Icon } from '@iconify/react'
 import BillingCard from '@/components/rendered/product/billingCard'
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+} from "@/components/ui/input-otp"
 
 const Page = () => {
     const [coupon, setCoupon] = useState({
@@ -12,6 +18,8 @@ const Page = () => {
     })
     const [savedCoupon, setSavedCoupon] = useState(false)
     const [billingList, setBillingList] = useState([])
+    const [pass, setPassword] = useState<number | null>()
+    const [authenticated, setAuthenticated] = useState<boolean>(false)
 
     useEffect(() => {
         const preCoupon = localStorage.getItem('coupon')
@@ -56,56 +64,90 @@ const Page = () => {
         localStorage.removeItem('coupon')
     }
 
+    const handleVerify = () => {
+        if (pass == 123456) {
+            setAuthenticated(true)
+            return
+        }
+        setPassword(null)
+        alert('Invalid pin entered')
+    }
+    useEffect(() => {
+        if (String(pass).length == 6)
+            handleVerify()
+    }, [pass])
+
     return (
-        <div>
-            <div>
-                Admin
-            </div>
-            <div>
-                <div className='p-1'>
-                    {
-                        savedCoupon ? <div className='bg-white p-5 rounded-xl shadow-lg'>
-                            <div className='flex justify-between'>
-                                <div>{coupon.name}</div>
-                                <div className='flex justify-around'>
-                                    <div className='mr-2'>
-                                        {coupon.quantity}
-                                    </div>
-                                    <div onClick={() => { handleCouponDelete() }}>
-                                        <Icon icon="material-symbols:delete" width="24" height="24" />
-                                    </div>
-                                </div>
+        <>
+            {
+                !authenticated ?<div className="p-4">
+                <div className="bg-white p-6 shadow-lg rounded-lg mt-6 max-w-sm mx-auto">
+                  <div className="text-lg font-semibold mb-4">Enter PIN</div>
+                  <InputOTP maxLength={6} onChange={(e) => setPassword(Number(e))}>
+                    <InputOTPGroup className="flex space-x-2">
+                      <InputOTPSlot index={0} className="w-12 h-12 border border-gray-300 rounded-lg text-center text-xl font-medium focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                      <InputOTPSlot index={1} className="w-12 h-12 border border-gray-300 rounded-lg text-center text-xl font-medium focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                      <InputOTPSlot index={2} className="w-12 h-12 border border-gray-300 rounded-lg text-center text-xl font-medium focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                      <InputOTPSlot index={3} className="w-12 h-12 border border-gray-300 rounded-lg text-center text-xl font-medium focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                      <InputOTPSlot index={4} className="w-12 h-12 border border-gray-300 rounded-lg text-center text-xl font-medium focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                      <InputOTPSlot index={5} className="w-12 h-12 border border-gray-300 rounded-lg text-center text-xl font-medium focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    </InputOTPGroup>
+                  </InputOTP>
+                </div>
+              </div>
+                    :
+
+                    <div>
+                        <div>
+                            Admin
+                        </div>
+                        <div>
+                            <div className='p-1'>
+                                {
+                                    savedCoupon ? <div className='bg-white p-5 rounded-xl shadow-lg'>
+                                        <div className='flex justify-between'>
+                                            <div>{coupon.name}</div>
+                                            <div className='flex justify-around'>
+                                                <div className='mr-2'>
+                                                    {coupon.quantity}
+                                                </div>
+                                                <div onClick={() => { handleCouponDelete() }}>
+                                                    <Icon icon="material-symbols:delete" width="24" height="24" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> :
+                                        <div className='p-2'>
+                                            <div>
+                                                <Input name='name' placeholder='Coupon Code' value={coupon.name} onChange={(e) => handleChange(e)} />
+                                            </div>
+                                            <div className='mt-2'>
+                                                <Input name='quantity' placeholder='Quantity limit' value={coupon.quantity} onChange={(e) => handleChange(e)} type='number' />
+                                            </div>
+                                            <div className='flex justify-end mt-2'>
+                                                <Button className='rounded-xl' variant={'outline'}>Cancel</Button>
+                                                <Button onClick={() => handleCouponSave()} className='rounded-xl'>Save</Button>
+                                            </div>
+                                        </div>
+                                }
                             </div>
-                        </div> :
-                            <div className='p-2'>
-                                <div>
-                                    <Input name='name' placeholder='Coupon Code' value={coupon.name} onChange={(e) => handleChange(e)} />
-                                </div>
-                                <div className='mt-2'>
-                                    <Input name='quantity' placeholder='Quantity limit' value={coupon.quantity} onChange={(e) => handleChange(e)} type='number' />
-                                </div>
-                                <div className='flex justify-end mt-2'>
-                                    <Button className='rounded-xl' variant={'outline'}>Cancel</Button>
-                                    <Button onClick={() => handleCouponSave()} className='rounded-xl'>Save</Button>
-                                </div>
+                        </div>
+                        <div>
+                            <div>
+                                List
                             </div>
-                    }
-                </div>
-            </div>
-            <div>
-                <div>
-                    List
-                </div>
-                <div>
-                    {
-                        billingList.map((billings, index) => {
-                            console.log(billings);
-                            return <BillingCard key={index} billings={billings} />
-                        })
-                    }
-                </div>
-            </div>
-        </div>
+                            <div>
+                                {
+                                    billingList.map((billings, index) => {
+                                        console.log(billings);
+                                        return <BillingCard key={index} billings={billings} />
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+            }
+        </>
     )
 }
 
